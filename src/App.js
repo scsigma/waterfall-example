6b82feee57d8f0e9e7dfea8b3663e56b0f1f65cc
scsigma <sullyclark@clarks01-sigma.local>
2023-02-02 14:10:29 -0800

    display example chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  useState,
+  useEffect,
+  useMemo
+} from 'react';
 
-function App() {
+import { client, useConfig, useElementData } from '@sigmacomputing/plugin';
+
+import Highcharts from 'highcharts';
+import HighchartsMore from "highcharts/highcharts-more";
+import HighchartsReact from 'highcharts-react-official';
+
+HighchartsMore(Highcharts);
+
+
+client.config.configureEditorPanel([
+  { name: "source", type: "element"},
+  { name: "open", type: "column", source: "source", allowMultiple: false},
+  { name: "high", type: "column", source: "source", allowMultiple: false},
+  { name: "low", type: "column", source: "source", allowMultiple: false},
+  { name: "close", type: "column", source: "source", allowMultiple: false},
+  { name: "date", type: "column", source: "source", allowMultiple: false},
+  { name: "volume", type: "column", source: "source", allowMultiple: false},
+  { name: "symbol", type: "column", source: "source", allowMultiple: false}
+]);
+
+
+const App = () => {
+  
+  const options = {
+    chart: {
+      type: 'waterfall'
+    },
+    series: [{
+        data: [{
+        //    x: 1,
+            y: 120000
+        }, {
+        //    x: 2,
+            y: 569000
+        }, {
+        //    x: 3,
+            y: 231000
+        },  {
+        //    x: 4,
+            y: -342000
+        }, {
+        //    x: 5,
+            y: -233000
+        }]        
+    }]
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options}/>
     </div>
   );
 }
